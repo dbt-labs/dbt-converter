583712af5e7b788fa895ab51e6c12da066e0265c
Devon Fulcher <devonfulcher3@gmail.com>
2023-11-06 14:31:22 -0600

    initial lookml implementation

--- converter/main.py
+++ converter/main.py
@@ -1,27 +1,48 @@
-from converter.spec_upgrade import MetricFLowConfig, to_yaml_mutli_file
-from converter.dbt_metrics_to_semantic_model_converter import write_semantic_models, write_metrics, delete_json_files, get_model
+from converter.lookml import lookml_to_semantic_manifest
+from converter.manifest_to_project import manifest_to_project
 import click
 import os
 
-@click.group(help='Convert dbt models and metrics to semantic models')
+from metricflow_parsing.dbt_dir_to_model import parse_dbt_project_to_model
+
+
+@click.group(help="Convert projects to dbt Semantic models")
 def cli():
     pass
 
-@cli.command(help= 'Given a dbt project path, converts metrics defined in the spec prior to dbt v1.6 to the spec defined in dbt v1.6')
-@click.option('--dbt-project-path', default= os.getcwd(), help='Path to dbt project containing models and metrics')
+
+@cli.command(
+    help="Given a dbt project path, converts metrics defined in the spec prior to dbt v1.6 to the spec defined in dbt v1.6"
+)
+@click.option(
+    "--dbt-project-path",
+    default=os.getcwd(),
+    help="Path to dbt project containing models and metrics",
+)
 def convert_metrics(dbt_project_path: str):
     try:
-        model = get_model(dbt_project_path)
+        model = parse_dbt_project_to_model(dbt_project_path)
     except Exception as e:
         return print(e)
-    write_semantic_models(model)
-    write_metrics(model)
+    manifest_to_project(model)
 
-    delete_json_files('metrics')
-    delete_json_files('data_source')
-    configs = MetricFLowConfig(os.path.join(os.getcwd(),'semantic_models'), os.path.join(os.getcwd(),'metrics'))
-    to_yaml_mutli_file(config=configs)
+
+"""
+@cli.command(help="Convert a LookML project to the dbt Semantic Layer")
+@click.option(
+    "--lookml-project-path",
+    default=os.getcwd(),
+    help="Path to dbt project containing models and metrics",
+)
+"""
+
+
+def convert_lookml(lookml_project_dir: str):
+    model = lookml_to_semantic_manifest(lookml_project_dir)
+    print(model)
+    manifest_to_project(model)
 
 
 if __name__ == "__main__":
-    cli()
+    # cli()
+    convert_lookml("/Users/devonfulcher/git/looker/dbtLabs.model.lkml")
