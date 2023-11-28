5a5a0daf85220e40337f844fb54ff724cb0f2d48
Devon Fulcher <devonfulcher3@gmail.com>
2023-11-28 13:46:08 -0600

    removed support for some measure properties

--- converter/lookml/measure.py
+++ converter/lookml/measure.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Dict, List, Optional
 from dbt_semantic_interfaces.implementations.elements.measure import PydanticMeasure
 from dbt_semantic_interfaces.type_enums.aggregation_type import AggregationType
 from pydantic import BaseModel
@@ -7,6 +7,8 @@ from pydantic import BaseModel
 class LkmlMeasure(BaseModel):
     name: str
     type: Optional[str]
+    filters: Optional[Dict[str, str]]
+    sql: Optional[str]
 
 
 def to_sl_measures(measures: Optional[List[LkmlMeasure]]) -> List[PydanticMeasure]:
@@ -37,6 +39,12 @@ def to_sl_measures(measures: Optional[List[LkmlMeasure]]) -> List[PydanticMeasur
     for measure in measures or []:
         if not measure.type:
             continue
+        # measures with filters aren't currently supported
+        if measure.filters:
+            continue
+        # measures with non-simple sql statements are not supported
+        if not is_simple_sql_property(measure.sql):
+            continue
         sl_measures.append(
             PydanticMeasure(
                 name=measure.name,
@@ -45,3 +53,9 @@ def to_sl_measures(measures: Optional[List[LkmlMeasure]]) -> List[PydanticMeasur
             )
         )
     return sl_measures
+
+
+def is_simple_sql_property(sql: str):
+    if not sql:
+        return True
+    return sql.startswith("$") and sql.count("$") == 1 and sql.endswith("}")
